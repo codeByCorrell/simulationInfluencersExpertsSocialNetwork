8a7a44ddb23a24dd87981ddb6a12304dbe7b8de1
Felix Correll <fcorrell@hm.edu>
2025-07-24 16:04:16 +0200

    cleaned up code

--- NetworkWindow.py
+++ NetworkWindow.py
@@ -3,10 +3,11 @@ import pyqtgraph as pg
 import numpy as np
 import random as rd
 from Agent import Agent
+from Simulation import Simulation
 
 class NetworkWindow(QWidget):
 
-    def __init__(self,simulation):
+    def __init__(self,simulation:Simulation):
         super().__init__()
         self.setWindowTitle("Network Simulation")
         self.showMaximized()
@@ -19,65 +20,21 @@ class NetworkWindow(QWidget):
 
     def createLayout(self):
 
+        # create an empty view and graph item
         win = pg.GraphicsLayoutWidget(show=True)
         self.view = win.addViewBox()
         self.view.setAspectLocked()
+        graph = pg.GraphItem()
 
-        graph = pg.GraphItem() 
-        # distribute agents randomly  
-        agents = list()     
-        pos = np.empty((0,2),dtype=int)
-        for i in range(self.sim.agents):
-            x = rd.randint(0,100)
-            y = rd.randint(0,100)
-            posPair = np.array([[x,y]])
-            if self.sim.influencers > i:
-                roleStr = "influencer"
-            elif self.sim.influencers + self.sim.experts > i:
-                roleStr = "expert"
-            else:
-                roleStr = "agent"
-            val = round(rd.uniform(0.00,1.00),2) if roleStr != "expert" else self.sim.truth
-            agents.append(Agent(posPair,roleStr,val,i))
-            pos = np.vstack([pos,posPair])
-        self.sim.agentsList = agents
-        
-        # connect agents randomly
-        adj = np.empty((0,2),dtype=int)
-        for i in range(self.sim.connections):
-            agent1Index = rd.randint(0,len(agents)-1)
-            agent2Index = rd.randint(0,len(agents)-1)
-            agent1 = agents[agent1Index]
-            agent2 = agents[agent2Index]
-            if len(agent2.followers) < self.sim.connLim - 1 and agent1Index != agent2Index:
-                agent1.rolemodels[agent2] = 0
-                agent2.followers.append(agent1)
-                newConn = np.array([[agent1Index,agent2Index]])
-            else:
-                agent1.ownOpinionMatters = True
-                newConn = np.array([[agent1Index,agent1Index]])
-            adj = np.vstack([adj,newConn])
+        # add agents, connect them randomly and create influencers
+        self.sim.addAgents()
+        self.sim.connectAgents()
+        self.sim.createInfluencers()
 
-        # creating influencers
-        for agent in agents:
-            if agent.role == "influencer":
-                followerCounter = 0
-                while followerCounter != self.sim.connLim:
-                    rdAgentId = rd.randint(0,len(agents)-1)
-                    newFollower = agents[rdAgentId]
-                    if newFollower not in agent.followers:
-                        newConn = np.array([[rdAgentId,agent.index]])
-                        adj = np.vstack([adj,newConn])
-                        agent.followers.append(newFollower)
-                        newFollower.rolemodels[agent] = 0
-                        followerCounter += 1
-        self.sim.connectionsList = adj
-
-
-        # Provide dummy symbols so nodes are visible
+        # draw the actual graph
         graph.setData(
-            pos=pos,
-            adj=adj,
+            pos=self.sim.positions,
+            adj=self.sim.connectionsList,
             size=40,
             symbol='o',
             pxMode=True,
@@ -85,61 +42,12 @@ class NetworkWindow(QWidget):
         )
         self.view.addItem(graph)
 
+        # draw the agents opinions, how much they weigh other opinions and who they are listening to
+        self.drawAgentValues()
+        self.sim.calculateListeningWeights()
+        self.drawArrowsAndWeights()
 
-        # Add labels as TextItem
-        for agent in agents:
-            self.writeAgentValue()
-            summe = 0 if not agent.ownOpinionMatters else 1
-            for key in agent.rolemodels.keys():
-                if key.role == "influencer":
-                    summe += 2
-                else:
-                    summe += 1
-            if summe != 0:
-                # influencers per default use only 50% to listen to other agents
-                if agent.role == "influencer":
-                    listensTo = 0.5
-                else:
-                    listensTo = 1
-                basicVal = round(listensTo/summe,2)
-            else:
-                basicVal = 1
-            agent.weightOwnOpinion = basicVal if agent.ownOpinionMatters else 0
-            for key in agent.rolemodels.keys():
-                if key.role == "influencer":
-                    agent.rolemodels[key] = 2 * basicVal
-                else:
-                    agent.rolemodels[key] = basicVal
-        self.sim.average = self.sim.getAverage()
-        self.sim.averages.append(self.sim.average)
-
-        # add arrows to indicate direction of connection
-        for ag1Id, ag2Id in adj:
-            p1 = pos[ag1Id]
-            p2 = pos[ag2Id]
-            dx, dy = p2 - p1
-            # check if self loop
-            if dx == 0 and dy == 0:
-                brushCol = 'r'
-                arrowPos = p1 - np.array([0.5,0])
-                textPos = p1 - np.array([0.7,0.2])
-            else:
-                brushCol = 'b'
-                arrowPos = p1 + 0.95 * (p2 - p1)
-                textPos = p1 + 0.5 * (p2 - p1)
-            angle = 180 - float(np.degrees(np.arctan2(dy, dx)))
-            arrow = pg.ArrowItem(pos=arrowPos, angle=angle, headLen=30, brush=brushCol)
-            self.view.addItem(arrow)
-            
-            agent1 = agents[ag1Id]
-            agent2 = agents[ag2Id]
-            weightVal = agent1.rolemodels[agent2] if agent1 != agent2 else agent1.weightOwnOpinion
-            text = pg.TextItem(str(weightVal), anchor=(0.5, 0.5), color='w')
-            text.setPos(textPos[0],textPos[1])
-            self.view.addItem(text)
-
-
-        # Buttons Section
+        # Add Buttons
         self.nextStepButton = QPushButton("Next Step",self)
         self.nextStepButton.setStyleSheet("background-color: blue; color: white")
         self.nextStepButton.clicked.connect(self.sim.nextStep)
@@ -166,7 +74,7 @@ class NetworkWindow(QWidget):
         buttonLay.addWidget(self.plotButton,3,0,1,2)
         buttonLay.addWidget(self.stopButton,4,0,1,2)
       
-        # Labels Section
+        # Add Labels
         self.truthLabel = QLabel("Truth: ",self)
         self.truthLabel.setText(f"Truth: {self.sim.truth}")
         self.truthLabel.setStyleSheet("color: blue")
@@ -179,13 +87,14 @@ class NetworkWindow(QWidget):
         self.labelLay.addWidget(self.avgLabel)
         self.labelLay.addWidget(self.stepsLabel)
 
+        # Create the layout
         self.layout = QVBoxLayout()
         self.layout.addWidget(win)
         self.layout.addLayout(buttonLay)
         self.layout.addLayout(self.labelLay)
         self.setLayout(self.layout)
 
-    def writeAgentValue(self):
+    def drawAgentValues(self):
         for text in self.viewTextItems:
             self.view.removeItem(text)
         self.viewTextItems = list()
@@ -198,6 +107,31 @@ class NetworkWindow(QWidget):
     def updateWindow(self):
         self.stepsLabel.setText(f"Steps: {self.sim.step}")
         self.avgLabel.setText(f"Average: {self.sim.average}")
-        self.writeAgentValue()
+        self.drawAgentValues()
+
+    def drawArrowsAndWeights(self):
+        for ag1Id, ag2Id in self.sim.connectionsList:
+            p1 = self.sim.positions[ag1Id]
+            p2 = self.sim.positions[ag2Id]
+            dx, dy = p2 - p1
+            # check if self loop
+            if dx == 0 and dy == 0:
+                brushCol = 'r'
+                arrowPos = p1 - np.array([0.5,0])
+                textPos = p1 - np.array([0.7,0.2])
+            else:
+                brushCol = 'b'
+                arrowPos = p1 + 0.95 * (p2 - p1)
+                textPos = p1 + 0.5 * (p2 - p1)
+            angle = 180 - float(np.degrees(np.arctan2(dy, dx)))
+            arrow = pg.ArrowItem(pos=arrowPos, angle=angle, headLen=30, brush=brushCol)
+            self.view.addItem(arrow)
+            
+            agent1 = self.sim.agentsList[ag1Id]
+            agent2 = self.sim.agentsList[ag2Id]
+            weightVal = agent1.rolemodels[agent2] if agent1 != agent2 else agent1.weightOwnOpinion
+            text = pg.TextItem(str(weightVal), anchor=(0.5, 0.5), color='w')
+            text.setPos(textPos[0],textPos[1])
+            self.view.addItem(text)
 
         
--- Simulation.py
+++ Simulation.py
@@ -1,5 +1,8 @@
 from PyQt5.QtCore import pyqtSignal,QObject,QCoreApplication
 import matplotlib.pyplot as plt
+import numpy as np
+import random as rd
+from Agent import Agent
 
 class Simulation(QObject):
     updatedValues = pyqtSignal()
@@ -18,6 +21,8 @@ class Simulation(QObject):
         self.steps = [0]
         self.average = 0
         self.averages = list()
+        self.connectionsList = np.empty((0,2),dtype=int)
+        self.positions = np.empty((0,2),dtype=int)
 
     def getAverage(self):
         if len(self.agentsList) == 0:
@@ -54,3 +59,73 @@ class Simulation(QObject):
 
     def stopSimulation(self):
         QCoreApplication.quit()
+
+    def addAgents(self):     
+        for i in range(self.agents):
+            x = rd.randint(0,100)
+            y = rd.randint(0,100)
+            posPair = np.array([[x,y]])
+            if self.influencers > i:
+                roleStr = "influencer"
+            elif self.influencers + self.experts > i:
+                roleStr = "expert"
+            else:
+                roleStr = "agent"
+            val = round(rd.uniform(0.00,1.00),2) if roleStr != "expert" else self.truth
+            self.agentsList.append(Agent(posPair,roleStr,val,i))
+            self.positions = np.vstack([self.positions,posPair])
+
+    def connectAgents(self):
+        for i in range(self.connections):
+            agent1Index = rd.randint(0,len(self.agentsList)-1)
+            agent2Index = rd.randint(0,len(self.agentsList)-1)
+            agent1 = self.agentsList[agent1Index]
+            agent2 = self.agentsList[agent2Index]
+            if len(agent2.followers) < self.connLim - 1 and agent1Index != agent2Index:
+                agent1.rolemodels[agent2] = 0
+                agent2.followers.append(agent1)
+                newConn = np.array([[agent1Index,agent2Index]])
+            else:
+                agent1.ownOpinionMatters = True
+                newConn = np.array([[agent1Index,agent1Index]])
+            self.connectionsList = np.vstack([self.connectionsList,newConn])
+
+    def createInfluencers(self):
+        for agent in self.agentsList:
+            if agent.role == "influencer":
+                followerCounter = 0
+                while followerCounter != self.connLim:
+                    rdAgentId = rd.randint(0,len(self.agentsList)-1)
+                    newFollower = self.agentsList[rdAgentId]
+                    if newFollower not in agent.followers:
+                        newConn = np.array([[rdAgentId,agent.index]])
+                        self.connectionsList = np.vstack([self.connectionsList,newConn])
+                        agent.followers.append(newFollower)
+                        newFollower.rolemodels[agent] = 0
+                        followerCounter += 1
+
+    def calculateListeningWeights(self):
+        for agent in self.agentsList:
+            summe = 0 if not agent.ownOpinionMatters else 1
+            for key in agent.rolemodels.keys():
+                if key.role == "influencer":
+                    summe += 2
+                else:
+                    summe += 1
+            if summe != 0:
+                # influencers per default use only 50% to listen to other agents
+                if agent.role == "influencer":
+                    listensTo = 0.5
+                else:
+                    listensTo = 1
+                basicVal = round(listensTo/summe,2)
+            else:
+                basicVal = 1
+            agent.weightOwnOpinion = basicVal if agent.ownOpinionMatters else 0
+            for key in agent.rolemodels.keys():
+                if key.role == "influencer":
+                    agent.rolemodels[key] = 2 * basicVal
+                else:
+                    agent.rolemodels[key] = basicVal
+        self.average = self.getAverage()
+        self.averages.append(self.average)
